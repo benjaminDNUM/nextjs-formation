056ab1ee5d5cd4d346d00fd7bc2db4d8a4f1571b
benjamin <benjamin.pinard@sg.social.gouv.fr>
2024-06-11 16:46:43 +0200

    test avec le fetch de books

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import BookList from '@/components/book/BookList'
 import { Book } from '@/types'
 
 const getBooks = async (): Promise<Book[]> => {
-  /*  const books = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/api/books`, {
+  const books = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/api/books`, {
     headers: { 'Content-Type': 'application/json' },
     cache: 'no-cache',
   })
-  console.log('log pour la prod', await books.json())*/
-  return []
+  return await books.json()
 }
 export default async function Home() {
-  const books = await getBooks()
-  return <BookList books={books} />
+  try {
+    const books = await getBooks()
+    return <BookList books={books} />
+  } catch (e) {
+    return <BookList books={[]} />
+  }
 }
